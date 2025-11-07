a123a49d2e3eb1e13b84566dac48cf560d3650fa
Timothé <timothe.cernon@gmail.com>
2025-11-07 11:17:43 +0100

    install ngzorro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { fr_FR, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(fr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideNzI18n(fr_FR), provideAnimationsAsync(), provideHttpClient()
   ]
 };
