c0836393269f8960a852f38c56484dd29072dcc3
Timothé <timothe.cernon@gmail.com>
2025-11-10 15:55:51 +0100

    form user et author

--- src/app/pages/authors/authors.ts
+++ src/app/pages/authors/authors.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {NzFormModule} from "ng-zorro-antd/form";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NzInputDirective} from "ng-zorro-antd/input";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzFlexDirective} from "ng-zorro-antd/flex";
 
 @Component({
   selector: 'app-authors',
-  imports: [],
+  imports: [NzFormModule, NzInputDirective, ReactiveFormsModule, NzButtonComponent, NzFlexDirective],
   templateUrl: './authors.html',
   styleUrl: './authors.css',
 })
 export class Authors {
+    authorForm = new FormGroup({
+        name: new FormControl<string>(null, [Validators.required]),
+        firstname: new FormControl<string>(null, [Validators.required]),
+    })
 
+    submitForm() {
+        // Pour annuler si le formulaire est invalide
+        //if (this.createAuthorForm.invalid) return;
+
+        // Pour obtenir la valeur du formulaire
+        console.log(this.authorForm.getRawValue())
+
+        // Pour vider le formulaire
+        this.authorForm.reset()
+    }
 }
--- src/app/pages/users/users.ts
+++ src/app/pages/users/users.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import {NzFormModule} from "ng-zorro-antd/form";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NzInputDirective} from "ng-zorro-antd/input";
+import {NzFlexDirective} from "ng-zorro-antd/flex";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+
 
 @Component({
   selector: 'app-users',
-  imports: [],
+    imports: [NzFormModule, ReactiveFormsModule, NzInputDirective, NzFlexDirective, NzButtonComponent],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
 export class Users {
+    userForm = new FormGroup({
+        FirstName: new FormControl<string>(null, [Validators.required]),
+        LastName: new FormControl<string>(null, [Validators.required]),
+        Email: new FormControl<string>(null, [Validators.required,Validators.email]),
+        BirthDate: new FormControl(null, [Validators.required]),
+    })
 
+    submitForm() {
+        // Pour annuler si le formulaire est invalide
+        //if (this.createAuthorForm.invalid) return;
+
+        // Pour obtenir la valeur du formulaire
+        console.log(this.userForm.getRawValue())
+
+        // Pour vider le formulaire
+        this.userForm.reset()
+    }
 }
