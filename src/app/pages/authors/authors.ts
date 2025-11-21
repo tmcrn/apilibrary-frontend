197c5ba8ae14534fa898453dd8009d51a56ec6f4
Timothé <timothe.cernon@gmail.com>
2025-11-21 09:02:10 +0100

    implémentation affichage author

--- src/app/pages/authors/authors.ts
+++ src/app/pages/authors/authors.ts
@@ -1,18 +1,41 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {NzFormModule} from "ng-zorro-antd/form";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NzInputDirective} from "ng-zorro-antd/input";
 import {NzButtonComponent} from "ng-zorro-antd/button";
 import {NzFlexDirective} from "ng-zorro-antd/flex";
 import {FormValidatorService} from "../../services/form-validator.service";
+import {NzTableModule} from "ng-zorro-antd/table";
+import {AuthorsService, GetAuthorDto} from "../../services/api";
+import {firstValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-authors',
-  imports: [NzFormModule, NzInputDirective, ReactiveFormsModule, NzButtonComponent, NzFlexDirective],
+    imports: [NzFormModule, NzInputDirective, ReactiveFormsModule, NzButtonComponent, NzFlexDirective, NzTableModule],
   templateUrl: './authors.html',
   styleUrl: './authors.css',
 })
-export class Authors {
+export class Authors implements OnInit {
+    private authorsService = inject(AuthorsService)
+
+    authors = signal<GetAuthorDto[]>([]);
+
+    constructor() {
+        console.log('Page author affichée');
+    }
+
+    async ngOnInit(){
+        await this.fetchAuthors();
+    }
+
+    async fetchAuthors() {
+        const authors = await firstValueFrom(this.authorsService.getAllAuthorsEndpoint());
+        this.authors.set(authors);
+    }
+
+
+
+
     private formValidator = inject(FormValidatorService);
 
     authorForm = new FormGroup({
@@ -32,4 +55,6 @@ export class Authors {
         // Pour vider le formulaire
         this.authorForm.reset()
     }
+
+
 }
